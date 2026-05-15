9bc78abedba5c92a357355f534d9f410edd64d03
seongbin45 <choiseongbin45@gmail.com>
2026-05-16 02:33:31 +0900

    Update config.py

--- finfit_youth/config.py
+++ finfit_youth/config.py
@@ -32,66 +32,12 @@ def _safe_path(v: str, default: str) -> str:
     return v
 
 
-
-YOUTH_API_BASE_URL = _safe_base_url(
-    _secret("YOUTH_API_BASE_URL", "https://www.youthcenter.go.kr")
-)
+YOUTH_API_BASE_URL = _safe_base_url(_secret("YOUTH_API_BASE_URL", "https://www.youthcenter.go.kr"))
 YOUTH_API_KEY = _secret("YOUTH_API_KEY", "")
 
-YOUTH_POLICY_LIST_PATH = _safe_path(
-    _secret("YOUTH_POLICY_LIST_PATH", "/opi/youthPlcyList.do"),
-    "/opi/youthPlcyList.do",
-)
-YOUTH_SPACE_LIST_PATH = _safe_path(
-    _secret("YOUTH_SPACE_LIST_PATH", "/opi/youthSpaceList.do"),
-    "/opi/youthSpaceList.do",
-)
-YOUTH_CONTENT_LIST_PATH = _safe_path(
-    _secret("YOUTH_CONTENT_LIST_PATH", "/opi/youthContentList.do"),
-    "/opi/youthContentList.do",
-)
-
-YOUTH_POLICY_DETAIL_PATH = _safe_path(
-    _secret("YOUTH_POLICY_DETAIL_PATH", "/opi/youthPlcyDtl.do"),
-    "/opi/youthPlcyDtl.do",
-)
-YOUTH_SPACE_DETAIL_PATH = _safe_path(
-    _secret("YOUTH_SPACE_DETAIL_PATH", "/opi/youthSpaceDtl.do"),
-    "/opi/youthSpaceDtl.do",
-)
-YOUTH_CONTENT_DETAIL_PATH = _safe_path(
-    _secret("YOUTH_CONTENT_DETAIL_PATH", "/opi/youthContentDtl.do"),
-    "/opi/youthContentDtl.do",
-)
-YOUTH_API_BASE_URL = _safe_base_url(
-    _secret("YOUTH_API_BASE_URL", "https://www.youthcenter.go.kr")
-)
-YOUTH_API_KEY = _secret("YOUTH_API_KEY", "")
-
-YOUTH_POLICY_LIST_PATH = _safe_path(
-    _secret("YOUTH_POLICY_LIST_PATH", "/opi/youthPlcyList.do"),
-    "/opi/youthPlcyList.do",
-)
-YOUTH_SPACE_LIST_PATH = _safe_path(
-    _secret("YOUTH_SPACE_LIST_PATH", "/opi/youthSpaceList.do"),
-    "/opi/youthSpaceList.do",
-)
-YOUTH_CONTENT_LIST_PATH = _safe_path(
-    _secret("YOUTH_CONTENT_LIST_PATH", "/opi/youthContentList.do"),
-    "/opi/youthContentList.do",
-)
-
-YOUTH_POLICY_DETAIL_PATH = _safe_path(
-    _secret("YOUTH_POLICY_DETAIL_PATH", "/opi/youthPlcyDtl.do"),
-    "/opi/youthPlcyDtl.do",
-)
-YOUTH_SPACE_DETAIL_PATH = _safe_path(
-    _secret("YOUTH_SPACE_DETAIL_PATH", "/opi/youthSpaceDtl.do"),
-    "/opi/youthSpaceDtl.do",
-)
-YOUTH_CONTENT_DETAIL_PATH = _safe_path(
-    _secret("YOUTH_CONTENT_DETAIL_PATH", "/opi/youthContentDtl.do"),
-    "/opi/youthContentDtl.do",
-)
+HTTP_TIMEOUT_SECONDS = float(_secret("YOUTH_API_TIMEOUT_SECONDS", "8"))
+HTTP_MAX_RETRIES = int(_secret("YOUTH_API_MAX_RETRIES", "3"))
+HTTP_BACKOFF_SECONDS = float(_secret("YOUTH_API_BACKOFF_SECONDS", "0.7"))
+HTTP_RATE_LIMIT_PER_MINUTE = int(_secret("YOUTH_API_RATE_LIMIT_PER_MINUTE", "120"))
 
 
