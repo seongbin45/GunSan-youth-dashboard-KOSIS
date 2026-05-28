f30a28501dd4ff99727840ce6b119344a825931e
최성빈 <choiseongbin45@gmail.com>
2026-05-29 02:15:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -618,7 +618,7 @@ def load_data():
         health_df = pd.DataFrame() # tables 변수를 활용한 health_df 초기화
         difficulty_df = None # tables 변수를 활용한 difficulty_df 초기화
         room_df = pd.DataFrame() # tables 변수를 활용한 room_df 초기화
-        saving_df = pd.DataFrame() # [수정 사항] 기존에 누락되었던 saving_df를 여기에 추가하여 초기화합니다!
+        saving_df = pd.DataFrame() #/#/# [수정 사항] 기존에 누락되었던 saving_df를 여기에 추가하여 초기화합니다! #/#/#
         job_df = pd.DataFrame({"DB 내 실제 테이블 목록": tables}) # tables 변수를 활용한 job_df 초기화
 
     conn.close() 
