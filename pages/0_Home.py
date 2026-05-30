b556dec73294c94451444297daeeb981f4644947
최성빈 <choiseongbin45@gmail.com>
2026-05-31 03:33:48 +0900

    Update 0_Home.py

--- pages/0_Home.py
+++ pages/0_Home.py
@@ -472,9 +472,9 @@ with col3:
             </div>
             <div class="card-features">
                 <span class="card-features-title">AI가 도와줄 수 있어요</span>
-                <div class="feature-item">업데이트중입니다...</div>
-                <div class="feature-item">업데이트중입니다...</div>
-                <div class="feature-item">업데이트중입니다...</div>
+                <div class="feature-item">저축 단계별 가이드 제공</div>
+                <div class="feature-item">몇 개월 후 저축 목표 시뮬레이션</div>
+                <div class="feature-item">전체 저축 단계 비교</div>
             </div>
         </div>
     </a>
