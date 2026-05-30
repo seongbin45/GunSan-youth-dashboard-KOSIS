3442e12c9bf0163b37b29ebe4967412b300dbfb2
최성빈 <choiseongbin45@gmail.com>
2026-05-30 22:49:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ with col1:
 with col2:
     # col2도 똑같이 전체 클릭 기능을 넣고 싶다면 아래처럼 <a> 태그로 감싸주면 됩니다.
     st.markdown("""
-    <a href="/?page=benefit" target="_self" style="text-decoration: none; color: inherit;">
+    <a href="/?page=finance" target="_self" style="text-decoration: none; color: inherit;">
         <div class="feature-card card-teal">
             <div class="card-icon">📚</div>
             <div class="card-title">금융 용어</div>
