e76fd62f615a208f7b13c815813b7c80c142d24f
최성빈 <choiseongbin45@gmail.com>
2026-06-01 02:20:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -511,7 +511,7 @@ with col3:
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 ### github.com 주소 뒤에 ?raw=true 추가
-github_image_url = "https://github.com/seongbin45/GunSan-youth-dashboard-KOSIS/blob/2ab6917c0e547dc2b59a1be6ac5a7ccddb3fdb15/tests/Image/%EC%A0%9C%EB%AA%A9%EC%9D%84%20%EC%9E%85%EB%A0%A5%ED%95%B4%EC%A3%BC%EC%84%B8%EC%9A%94.%20(2).png?raw=true"
+github_image_url = "https://github.com/seongbin45/GunSan-youth-dashboard-KOSIS/blob/bce7f88534150548c7a1428aa516eb1f5a7f6b86/tests/Image/%EC%A0%9C%EB%AA%A9%EC%9D%84%20%EC%9E%85%EB%A0%A5%ED%95%B4%EC%A3%BC%EC%84%B8%EC%9A%94.%20(5).png?raw=true"
 
 ### 스트림릿에 이미지 출력
 st.image(github_image_url, use_container_width=True)
