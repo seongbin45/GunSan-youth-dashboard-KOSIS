edb7b4298b857a8cbb213235bc6ca61f284f8222
최성빈 <choiseongbin45@gmail.com>
2026-06-03 21:12:14 +0900

    Update footer content and add survey link
    Removed email from footer and added survey link button.

--- pages/5_금융용어.py
+++ pages/5_금융용어.py
@@ -281,9 +281,12 @@ st.markdown("""
 st.markdown("""
 <div class="footer">
     <p>💌 저희는 더 나은 경험을 드리기 위해 꾸준히 준비 중입니다. 소중한 의견을 들려주시면 서비스 개선에 큰 도움이 됩니다.</p>
-    <p>🔒 개인정보 보호 | 📞 문의: 010-4666-9672 | 📧 choiseongbin45@gmail.com</p>
+    <p>🔒 개인정보 보호 | 📞 문의: 010-4666-9672 |</p>
 </div>
-""", unsafe_allow_html=True)
+"""
+   '<a href="https://docs.google.com/forms/d/e/1FAIpQLSco-O4cGhbt1iMOwrEkqX5Vt0-8ctAtCxM5Z6JjmFlP-Uqq-Q/viewform?usp=header" target="_blank"><button style="color: blue;">설문 링크로 이동</button></a>', 
+    unsafe_allow_html=True  
+            , unsafe_allow_html=True)
 
 
 
