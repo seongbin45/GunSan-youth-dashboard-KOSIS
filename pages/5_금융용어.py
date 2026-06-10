3aed686caf5f24bca720d9f0a3c7705f38e4c609
최성빈 <choiseongbin45@gmail.com>
2026-06-11 05:08:36 +0900

    Update 5_금융용어.py

--- pages/5_금융용어.py
+++ pages/5_금융용어.py
@@ -282,7 +282,7 @@ st.markdown("""
 <div class="footer">
     <p>💌 저희는 더 나은 경험을 드리기 위해 꾸준히 준비 중입니다. 소중한 의견을 들려주시면 서비스 개선에 큰 도움이 됩니다.</p>
     <p>🔒 개인정보 보호</p>
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSco-O4cGhbt1iMOwrEkqX5Vt0-8ctAtCxM5Z6JjmFlP-Uqq-Q/viewform?usp=header" target="_blank"><button style="color: blue;">설문 링크로 이동</button></a>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSco-O4cGhbt1iMOwrEkqX5Vt0-8ctAtCxM5Z6JjmFlP-Uqq-Q/viewform?usp=header" target="_blank"><button style="color: peach;">설문 링크로 이동</button></a>
 </div>
 """, unsafe_allow_html=True)
 
